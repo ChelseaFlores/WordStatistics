1ccd8d875b03534a0b0eff8f4db5ee8488010ee5
chelseaflores <chelseaflores@chelseas-mbp.lan>
2021-05-07 00:47:44 -0500

    Junit corrections, sonar properties and class files.

--- Word-Statistics/src/JUnitTest.java
+++ Word-Statistics/src/JUnitTest.java
@@ -6,48 +6,75 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class JUnitTest {
-
-	@Test
-	public void testFileLineCount() {
-		int lines = 0;
+	
+	protected Scanner scan1;
+	protected int lines;
+	
+	@Before
+	public void setUp1()
+	{
+		lines = 0;
 		try {
-			Scanner scan = new Scanner( new FileReader("TestText.txt") );
+			scan1 = new Scanner( new FileReader("TestText.txt") );
 			
-			while( scan.hasNextLine() )
+			while( scan1.hasNextLine() )
 			{
-				scan.nextLine();
+				scan1.nextLine();
 				lines++;
-			}	
-			scan.close();
-			
+			}				
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void testFileLineCount() {
 		assertEquals("Number of lines: ", 13, lines);
 	}
 	
-	@Test
-	public void testFileCharCount()
+	@After
+	public void tearDown1()
+	{
+		scan1.close();
+	}
+	
+	protected int charNum;
+	protected Scanner scan2;
+	
+	@Before
+	public void setUp2()
 	{
 		try {
-			Scanner scan = new Scanner( new FileReader("TestText.txt") );
-			int charNum = 0;
-			while( scan.hasNextLine() )
+			scan2 = new Scanner( new FileReader("TestText.txt") );
+			charNum = 0;
+			while( scan2.hasNextLine() )
 			{
-				String line = scan.nextLine();
+				String line = scan2.nextLine();
 				charNum = Driver.numOfChars(line, charNum);
 			}	
 			
 			charNum = charNum + 13; //13 is the number of lines. This line is present after the while loop in processData method.
 			
-			scan.close();
-			assertEquals("Number of lines: ", 450, charNum);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void testFileCharCount()
+	{
+		assertEquals("Number of lines: ", 450, charNum);
+	}
+	
+	@After
+	public void tearDown2()
+	{
+		scan2.close();
 	}
 	
 	@Test
@@ -59,12 +86,17 @@ public class JUnitTest {
 		assertEquals("Number of char: ", 64, noChar);
 	}
 	
-	@Test
-	public void testWordFrequency()
+	
+	protected HashMap<String, Integer> uniqueWordCountTest;
+	protected HashMap<String, Integer> uniqueWordCountRes;
+	protected ArrayList<String> arr;
+	
+	@Before
+	public void setUp3()
 	{
-		HashMap<String, Integer> uniqueWordCountTest = new HashMap<String, Integer>();
-		HashMap<String, Integer> uniqueWordCountRes = new HashMap<String, Integer>();
-		ArrayList<String> arr = new ArrayList<String>();
+		uniqueWordCountTest = new HashMap<String, Integer>();
+		uniqueWordCountRes = new HashMap<String, Integer>();
+		arr = new ArrayList<String>();
 		
 		arr.add("A");
 		arr.add("A");
@@ -89,21 +121,28 @@ public class JUnitTest {
 		uniqueWordCountTest.put("ARE", 1);
 		uniqueWordCountTest.put("BEHAVE", 3);
 		uniqueWordCountTest.put("BREAKS", 4);
-		
-		assertTrue(uniqueWordCountTest.equals(uniqueWordCountRes));
-	
 	}
 	
 	@Test
-	public void testReplace()
+	public void testWordFrequency()
 	{
-		HashMap<Integer, String> replacedLinesTest = new HashMap<Integer, String>();
+		assertTrue(uniqueWordCountTest.equals(uniqueWordCountRes));
+	}
+	
+	protected HashMap<Integer, String> replacedLinesTest;
+	protected HashMap<Integer, String> replacedLinesRes;
+	protected Scanner scan;
+	
+	@Before
+	public void setUp()
+	{
+		replacedLinesTest = new HashMap<Integer, String>();
 		replacedLinesTest.put(1, "This is a result file that we will use for junit Testing!");
 		replacedLinesTest.put(2, "Test should not match lowercase result for the replace");
 		
-		HashMap<Integer, String> replacedLinesRes = new HashMap<Integer, String>();
+		replacedLinesRes = new HashMap<Integer, String>();
 		try {
-			Scanner scan = new Scanner( new FileReader("TestText.txt") );
+			scan = new Scanner( new FileReader("TestText.txt") );
 			int lineNo = 0;
 			while( scan.hasNextLine() )
 			{
@@ -111,13 +150,21 @@ public class JUnitTest {
 				lineNo++;	
 				replacedLinesRes = Driver.replaceWord(lineNo, line, "test", "result", replacedLinesRes);
 			}	
-			scan.close();
 		
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void testReplace()
+	{
 		assertTrue(replacedLinesTest.equals(replacedLinesRes));
 	}
 	
-
+	@After
+	public void tearDown()
+	{
+		scan.close();
+	}
 }
